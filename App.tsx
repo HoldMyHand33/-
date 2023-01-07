6fd089ff42fe940ed0f18ab5f5e346168a6a7518
BHyeonKim <rlaqhguse@gmail.com>
2023-01-07 14:18:44 +0900

    Init project

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>시작</Text>
+    </View>
+  );
+};
+
+export default App;
